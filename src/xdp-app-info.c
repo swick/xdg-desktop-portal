95d5c120e983bb43e1bfc2c8bc7c2014c09c9d7d
Jonas Ådahl <jadahl@gmail.com>
2025-01-14 11:08:15 +0100

    xdp/app-info: Log how app info was created

--- src/xdp-app-info.c
+++ src/xdp-app-info.c
@@ -794,14 +794,19 @@ xdp_connection_create_app_info_sync (GDBusConnection  *connection,
   g_autofd int pidfd = -1;
   uint32_t pid;
   g_autoptr(GError) local_error = NULL;
+  const char *app_info_kind = NULL;
 
   if (!xdp_connection_get_pidfd (connection, sender, cancellable, &pidfd, &pid, error))
     return NULL;
 
   app_info = maybe_create_test_app_info ();
+  app_info_kind = "test";
 
   if (app_info == NULL)
-    app_info = xdp_app_info_flatpak_new (pid, pidfd, &local_error);
+    {
+      app_info = xdp_app_info_flatpak_new (pid, pidfd, &local_error);
+      app_info_kind = "flatpak";
+    }
 
   if (!app_info && !g_error_matches (local_error, XDP_APP_INFO_ERROR,
                                      XDP_APP_INFO_ERROR_WRONG_APP_KIND))
@@ -812,7 +817,10 @@ xdp_connection_create_app_info_sync (GDBusConnection  *connection,
   g_clear_error (&local_error);
 
   if (app_info == NULL)
-    app_info = xdp_app_info_snap_new (pid, pidfd, &local_error);
+    {
+      app_info = xdp_app_info_snap_new (pid, pidfd, &local_error);
+      app_info_kind = "snap";
+    }
 
   if (!app_info && !g_error_matches (local_error, XDP_APP_INFO_ERROR,
                                      XDP_APP_INFO_ERROR_WRONG_APP_KIND))
@@ -823,9 +831,14 @@ xdp_connection_create_app_info_sync (GDBusConnection  *connection,
   g_clear_error (&local_error);
 
   if (app_info == NULL)
-    app_info = xdp_app_info_host_new (pid, pidfd);
+    {
+      app_info = xdp_app_info_host_new (pid, pidfd);
+      app_info_kind = "derived host";
+    }
 
   g_return_val_if_fail (app_info != NULL, NULL);
+
+  g_debug ("Adding %s app '%s'", app_info_kind, xdp_app_info_get_id (app_info));
 
   cache_insert_app_info (sender, app_info);
 
@@ -864,7 +877,7 @@ xdp_connection_create_host_app_info_sync (GDBusConnection  *connection,
       if (!xdp_app_info_is_host (detected_app_info))
         {
           g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
-                       "Can't manually register non-host applications");
+                       "Can't manually register non-host applications	");
           return NULL;
         }
 
@@ -872,6 +885,8 @@ xdp_connection_create_host_app_info_sync (GDBusConnection  *connection,
       if (!app_info)
         return NULL;
     }
+
+  g_debug ("Adding registered host app '%s'", xdp_app_info_get_id (app_info));
 
   cache_insert_app_info (sender, app_info);
 
