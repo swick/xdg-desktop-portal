d250e2358993293829176eef319c4b5ad6a4950b
Sebastian Wick <sebastian.wick@redhat.com>
2026-03-23 21:52:49 +0100

    app-info: Add a destroyed signal
    The XdpAppInfo represents a single sender and (ignoring temporary
    references) gets destroyed when the sender drops off the bus. That's a
    more targeted signal than subscribing to peer_died, and also again means
    we don't have to have a lookup from the sender name to the XdpAppInfo.

--- shared/xdp-app-info.c
+++ shared/xdp-app-info.c
@@ -59,6 +59,8 @@ typedef struct _XdpAppInfoPrivate
   /* misc */
   XdpAppInfoFlags flags;
   XdpEntitlements *entitlements;
+
+  gboolean disposed;
 } XdpAppInfoPrivate;
 
 static void g_initable_init_iface (GInitableIface *iface);
@@ -80,6 +82,14 @@ typedef enum
 } XdpAppInfoProps;
 
 static GParamSpec *properties [PROP_SENDER + 1];
+
+enum
+{
+  DESTROYED,
+  N_SIGNALS,
+};
+
+static guint signals[N_SIGNALS] = { 0 };
 
 static gboolean
 xdp_app_info_initable_init (GInitable     *initable,
@@ -127,6 +137,12 @@ xdp_app_info_dispose (GObject *object)
   XdpAppInfoPrivate *priv =
     xdp_app_info_get_instance_private (XDP_APP_INFO (object));
   g_autoptr(GError) error = NULL;
+
+  if (!priv->disposed)
+    {
+      g_signal_emit (object, signals[DESTROYED], 0);
+      priv->disposed = TRUE;
+    }
 
   g_clear_pointer (&priv->engine, g_free);
   g_clear_pointer (&priv->id, g_free);
@@ -307,6 +323,13 @@ xdp_app_info_class_init (XdpAppInfoClass *klass)
                          G_PARAM_STATIC_STRINGS);
 
   g_object_class_install_properties (object_class, G_N_ELEMENTS (properties), properties);
+
+  signals[DESTROYED] = g_signal_new ("destroyed",
+                                     G_TYPE_FROM_CLASS (klass),
+                                     G_SIGNAL_RUN_LAST,
+                                     0,
+                                     NULL, NULL, NULL,
+                                     G_TYPE_NONE, 0);
 }
 
 static void
